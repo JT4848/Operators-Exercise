f40b9a636c60bfb7fde51244f4d0ec62810c615a
John Thomas Pricone <jtpricone@gmail.com>
2022-11-28 11:13:16 -0500

    Operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,56 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int x = 12;
+            int y = 20;
+            x += y;
+            Console.WriteLine(x);
+
+            x = 15;
+            y = 10;
+            x -= y;
+            Console.WriteLine(x);
+
+            int a = 17;
+            int b = 4;
+            int q = a / b;
+            
+            
+            Console.WriteLine(x);
+
+            x = 50;
+            y = 80;
+            x *= y;
+            Console.WriteLine(x);
+
+
+
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"The radius of the circle is {radius} {Math.Round (area,2)}");
+
+
+
+
+
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         }
     }
 }
